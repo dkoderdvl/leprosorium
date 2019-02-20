b83a61642609c3d5ae45e4248bc9420a45aab08a
dkoderdvl <dkoder.dvl@gmail.com>
2019-02-20 12:11:31 +0300

    add db

--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ require 'sinatra/reloader'
 require 'sqlite3'
 
 def init_db
-  @db = SQLite3::Database.new 'leprsorium.sqlite'
+  @db = SQLite3::Database.new 'leprosorium.sqlite'
   @db.results_as_hash = true
 end
 
